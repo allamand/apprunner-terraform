a65704df63d254338bc76128dba805cdbea2424f
Sebastien Allamand <sallaman@amazon.com>
2022-05-01 17:59:51 +0000

    remove Dockerfile

--- terraform/app-runner.tf
+++ terraform/app-runner.tf
@@ -24,8 +24,7 @@ resource "aws_apprunner_service" "reviewapps" {
       code_configuration {
         code_configuration_values {
           #build_command = "npm install"
-          build_command = ["cd votingapp && pip install -r requirements.txt",
-          "groupadd -r restaurantgroup && useradd -r -g restaurantgroup restaurantuser"]
+          build_command = "cd votingapp && pip install -r requirements.txt"
           port          = var.port
           #runtime       = "NODEJS_12"
           runtime = "PYTHON_3"
